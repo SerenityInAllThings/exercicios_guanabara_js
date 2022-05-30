2bf982fcff00bff9431a394216b11b542fb0ae53
SerenityInAllThings <petersonvgama@gmail.com>
2022-05-30 19:15:15 -0300

    Adicionando cabeçalho ex.4

--- lista/04/Vincenzo-04.js
+++ lista/04/Vincenzo-04.js
@@ -1,10 +1,17 @@
-const prompt = require('prompt-sync')({})
+/* 4) Desenvolva um algoritmo que leia dois números inteiros e mostre 
+o somatório entre eles.
+Ex:
+Digite um valor: 8
+Digite outro valor: 5
+A soma entre 8 e 5 é igual a 13.*/
 
-const lerNumeroInteiro = () =>{
-    const numero1 = parseInt(prompt("Valor 1:"))
-    const numero2 = parseInt(prompt("Valor 2:"))
-    const somaNum1_2 = numero1 + numero2
+const prompt = require("prompt-sync")({});
 
-    console.log("A soma entre 8 e 5 é igual a",somaNum1_2.toString())
-}
-lerNumeroInteiro()
+const lerNumeroInteiro = () => {
+  const numero1 = parseInt(prompt("Valor 1:"));
+  const numero2 = parseInt(prompt("Valor 2:"));
+  const somaNum1_2 = numero1 + numero2;
+
+  console.log("A soma entre 8 e 5 é igual a", somaNum1_2.toString());
+};
+lerNumeroInteiro();
