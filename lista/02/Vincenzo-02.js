ba831ca0d1ee8b933bcf1c48198c1952266cceaa
SerenityInAllThings <petersonvgama@gmail.com>
2022-05-30 19:09:16 -0300

    Adicionando cabeçalho ex.2

--- lista/02/Vincenzo-02.js
+++ lista/02/Vincenzo-02.js
@@ -1,7 +1,16 @@
-const prompt = require('prompt-sync')({})
+/* 2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de 
+boas-vindas para ela:
 
-const lerNome = () =>{
-    const Nome = prompt("Qual é o seu nome?")
-    console.log("Olá",Nome,", é um prazer te conhecer!")
-}
-lerNome()
+### Ex:
+
+#### Qual é o seu nome? João da Silva
+
+#### Olá João da Silva, é um prazer te conhecer! */
+
+const prompt = require("prompt-sync")({});
+
+const lerNome = () => {
+  const Nome = prompt("Qual é o seu nome?");
+  console.log("Olá", Nome, ", é um prazer te conhecer!");
+};
+lerNome();
