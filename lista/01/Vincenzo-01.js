9daf29dd4cdcdcb29713faf4fbba334296a17bc6
SerenityInAllThings <petersonvgama@gmail.com>
2022-05-30 19:04:11 -0300

    Adicionando cabeçalho ex.1

--- lista/01/Vincenzo-01.js
+++ lista/01/Vincenzo-01.js
@@ -1,6 +1,7 @@
-const prompt = require('prompt-sync')({})
+// 1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+const prompt = require("prompt-sync")({});
 
-const Mensagem = () =>{
-    const MensagemTela = prompt("Olá Mundo")
-}
-Mensagem()
+const Mensagem = () => {
+  const MensagemTela = prompt("Olá Mundo");
+};
+Mensagem();
