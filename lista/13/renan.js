4b87449e104f493e26198819fd0edb27496e9773
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-06 18:02:02 -0300

    Adicionando cabeçalho do exercícios 13

--- lista/13/renan.js
+++ lista/13/renan.js
@@ -1,3 +1,6 @@
+/*13) Faça um algoritmo que leia o salário de um funcionário, 
+calcule e mostre o seu novo salário, com 15% de aumento.*/
+
 const prompt = require("prompt-sync")({})
 
 const lerSalario = () => {
