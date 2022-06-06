ea97601c144a82fdf71c590f8bed37c5770f6aaf
SerenityInAllThings <petersonvgama@gmail.com>
2022-06-06 18:04:24 -0300

    Adicionando o enunciado do exercício 14

--- lista/14/renan.js
+++ lista/14/renan.js
@@ -1,3 +1,8 @@
+/*14) A locadora de carros precisa da sua ajuda para cobrar seus serviços. Escreva
+um programa que pergunte a quantidade de Km percorridos por um carro alugado e a
+quantidade de dias pelos quais ele foi alugado. Calcule o preço total a pagar,
+sabendo que o carro custa R$90 por dia e R$0,20 por Km rodado.*/
+
 // 16) [DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um
 // fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele
 // já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule
